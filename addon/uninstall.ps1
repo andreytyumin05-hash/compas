9021f4216029b9834ac07f9b5b0f177c49dfe5d9
andreytyumin05-hash <andreytyumin05@gmail.com>
2026-09-05 16:14:39 +0300

    fix: cleanly remove AddIn runtime configuration

--- addon/uninstall.ps1
+++ addon/uninstall.ps1
@@ -1,10 +1,6 @@
 $ErrorActionPreference = 'Stop'
 
-$dllCandidates = @(
-    (Join-Path $PSScriptRoot 'csharp\bin\x64\Release\net48\CompasAiCad.dll'),
-    (Join-Path $PSScriptRoot 'csharp\bin\Release\CompasAiCad.dll')
-)
-$dll = $dllCandidates | Where-Object { Test-Path $_ } | Select-Object -First 1
+$dll = Join-Path $PSScriptRoot 'csharp\bin\x64\Release\net48\CompasAiCad.dll'
 $regasm = Join-Path $env:WINDIR 'Microsoft.NET\Framework64\v4.0.30319\RegAsm.exe'
 
 if ((Test-Path $dll) -and (Test-Path $regasm)) {
@@ -12,5 +8,8 @@ if ((Test-Path $dll) -and (Test-Path $regasm)) {
 }
 
 Remove-Item 'HKCU:\Software\ASCON\KOMPAS-3D\AddIns\CompasAiCad' -Recurse -Force -ErrorAction SilentlyContinue
+
+[Environment]::SetEnvironmentVariable('COMPAS_REPO', $null, 'User')
+[Environment]::SetEnvironmentVariable('COMPAS_PYTHON', $null, 'User')
 
 Write-Host 'AI CAD AddIn removed.' -ForegroundColor Green
