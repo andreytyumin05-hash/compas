964935c3cb27b1bb47dd67e07ae6ae8881ea7b08
andreytyumin05-hash <andreytyumin05@gmail.com>
2026-09-05 14:15:11 +0300

    Add add-in assembly metadata

---
+++ addon/csharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Compas AI CAD")]
+[assembly: AssemblyDescription("KOMPAS-3D AI CAD Add-In")]
+[assembly: AssemblyCompany("Compas Project")]
+[assembly: AssemblyProduct("Compas AI CAD")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: ComVisible(true)]
